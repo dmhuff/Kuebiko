ebccea803b17ad812695dd9579ccd379f69a0dd8
Dave Huffman <daveh303@yahoo.com>
2011-08-13 13:22:19 -0400

    Removed deprecated code (filtering logic). Added documentation. Added logic to generate unique default titles for new notes.

--- src/dmh/kuebiko/controller/NoteManager.java
+++ src/dmh/kuebiko/controller/NoteManager.java
@@ -6,18 +6,18 @@
 
 package dmh.kuebiko.controller;
 
-import static org.apache.commons.lang.StringUtils.isBlank;
-
 import java.io.IOException;
 import java.util.Collection;
 import java.util.Collections;
 import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import com.google.common.base.Predicate;
 import com.google.common.collect.Collections2;
-import com.google.common.collect.ImmutableList;
 import com.google.common.collect.Lists;
 
+import dmh.kuebiko.Main;
 import dmh.kuebiko.model.Note;
 import dmh.kuebiko.model.NoteDao;
 import dmh.kuebiko.model.ValidationException;
@@ -29,12 +29,18 @@ import dmh.kuebiko.util.NoteTitleFunction;
  * @author davehuffman
  */
 public class NoteManager {
+    static final String DEFAULT_NOTE_TITLE = "Untitled Note"; // TODO i18n.
+    
     private final NoteDao noteDao;
     
     private List<Note> notes = null;
 
     private final Collection<Note> deletedNotes;
     
+    /**
+     * Constructor.
+     * @param noteDao The DAO to use for note persistence.
+     */
     public NoteManager(NoteDao noteDao) {
         this.noteDao = noteDao;
         
@@ -44,19 +50,24 @@ public class NoteManager {
 
     private void loadAllNotes() {
         try {
-            notes = noteDao.readNotes();
+            notes = Lists.newArrayList(noteDao.readNotes());
         } catch (IOException e) {
             throw new DataStoreException("Could not read notes.", e);
         }
     }
     
+    /**
+     * @return An immutable view of all notes in the stack.
+     */
     public List<Note> getNotes() {
         return Collections.unmodifiableList(notes);
     }
     
+    /**
+     * @return A view containing the titles for all notes in the stack.
+     */
     public List<String> getNoteTitles() {
-        return Collections.unmodifiableList(
-                Lists.transform(notes, NoteTitleFunction.getInstance()));
+        return Lists.transform(notes, NoteTitleFunction.getInstance());
     }
     
     /**
@@ -74,7 +85,61 @@ public class NoteManager {
         return notes.get(index);
     }
     
-    public void addNote(Note newNote) {
+    /**
+     * Add a new note with a default title.
+     * @return The title of the new note.
+     */
+    public String addNewNote() {
+        final String title = genUniqueNoteTitle();
+        addNewNote(title);
+        return title;
+    }
+    
+    /**
+     * Generate a unique note title for a new note.
+     * @return A note title that is guaranteed to be unique among all notes
+     *         in the stack.
+     */
+    String genUniqueNoteTitle() {
+        // Get a list of default note titles.
+        final Collection<String> defaultTitles = 
+                Collections2.filter(getNoteTitles(), 
+                        new Predicate<String>() {
+                            @Override
+                            public boolean apply(String input) {
+                                return input.startsWith(DEFAULT_NOTE_TITLE);
+                            }
+                        });
+        
+        if (defaultTitles.size() == 0) {
+            return DEFAULT_NOTE_TITLE;
+        }
+        
+        // Default note titles exist; work thought the list and create a new, 
+        // unique default note title.
+        Pattern titleRegex = Pattern.compile(DEFAULT_NOTE_TITLE + " (\\d+)");
+        int maxSuffix = 1;
+        for (String title: defaultTitles) {
+            Matcher matcher = titleRegex.matcher(title);
+            if (matcher.matches()) {
+                int suffix = Integer.parseInt(matcher.group(1));
+                if (suffix > maxSuffix) {
+                    maxSuffix = suffix;
+                }
+            }
+        }
+        String title = String.format("%s %d", DEFAULT_NOTE_TITLE, maxSuffix + 1);
+        Main.log("genUniqueNoteTitle(); #=> [%s].", title);
+        return title;
+    }
+    
+    public void addNewNote(String title) {
+        Note note = new Note();
+        note.setTitle(title);
+        addNote(note);
+    }
+    
+    void addNote(Note newNote) {
         notes.add(newNote);
     }
     
