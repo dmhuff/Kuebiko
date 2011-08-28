9f83335df30f63733a463a4b304390ae27e9d9a9
Dave Huffman <daveh303@yahoo.com>
2011-08-27 22:31:57 -0400

    Initial commit.

---
+++ src/dmh/kuebiko/view/NewStackAction.java
@@ -0,0 +1,42 @@
+/**
+ * Kuebiko - NewStackAction.java
+ * Copyright 2011 Dave Huffman (daveh303 at yahoo dot com).
+ * TODO license info.
+ */
+package dmh.kuebiko.view;
+
+import static javax.swing.KeyStroke.getKeyStroke;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.InputEvent;
+import java.awt.event.KeyEvent;
+import java.util.Observable;
+
+/**
+ * TODO Document.
+ *
+ * @author davehuffman
+ */
+public class NewStackAction extends UnimplementedAction {
+    private static final long serialVersionUID = 1L;
+    
+    public NewStackAction() {
+        super("New Stack"); // TODO i18n.
+        
+        putValue(SHORT_DESCRIPTION, "Create a new stack of notes."); // TODO i18n.
+        putValue(LONG_DESCRIPTION, getValue(SHORT_DESCRIPTION));
+        putValue(ACCELERATOR_KEY, getKeyStroke(KeyEvent.VK_N, 
+                InputEvent.SHIFT_MASK | InputEvent.META_MASK));
+        putValue(MNEMONIC_KEY, KeyEvent.VK_T);
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent event) {
+        super.actionPerformed(event);
+    }
+
+    @Override
+    public void update(Observable o, Object arg) {
+        // Do nothing.
+    }
+}
