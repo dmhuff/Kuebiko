12fdd7d832143fa90f8422f68cac2e26e391ebe8
Dave Huffman <daveh303@yahoo.com>
2011-09-03 23:05:14 -0400

    Initial commit of skeleton for file system DAO for notes.

---
+++ src/dmh/kuebiko/model/FileSystemNoteDao.java
@@ -0,0 +1,16 @@
+/**
+ * Kuebiko - FileSystemNoteDao.java
+ * Copyright 2011 Dave Huffman (daveh303 at yahoo dot com).
+ * TODO license info.
+ */
+package dmh.kuebiko.model;
+
+/**
+ * Note data access object (DAO) for storing notes in the file system. This DAO
+ * will keep all note data in memory and persist
+ *
+ * @author davehuffman
+ */
+public class FileSystemNoteDao extends InMemoryNoteDao {
+
+}
